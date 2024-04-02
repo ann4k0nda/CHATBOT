18a59ec3e84edc86db656215ef524799d2aaf205
boumpatrak <146081518+ann4k0nda@users.noreply.github.com>
2024-04-02 17:30:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv/config');
+const {Client, IntentsBitField} = require('discord.js');
+const {OpenAI} = require("openai");
+
+const client = new Client({
+    intents: ["Guilds", "GuildMembers", "GuildMessages"],
+});
+
+client.on('ready', ()=> {
+    console.log("C'est bon !");
+})
+
+const openai = new OpenAI ({
+    apiKey: process.env.API_KEY
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.id !== process.env.CHANNEL_ID) return;
+    if (message.content.startsWith('!')) return;
+
+    let conversationLog = [{ role: 'system', content: "Tu es mon ami."}];
+
+    await message.channel.sendTyping();
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg)=> {
+        if (message.content.startsWith("!")) return;
+        if (msg.author.id !== client.user.id && message.author.bot) return;
+        if (msg.author.id !== message.author.id) return;
+
+        conversationLog.push ({
+            role : 'user',
+            content: msg.content,
+        })
+    })
+
+    const result = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+    })
+
+    message.reply(result.choices[0].message);
+});
+
+client.login(process.env.TOKEN);
